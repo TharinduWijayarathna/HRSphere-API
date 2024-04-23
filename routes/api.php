b361460be89a30d289f1afdad2886a3023af1e55
TharinduWijayarathna <tharinduwijayarathne87@gmail.com>
2024-04-23 15:56:33 +0530

    Downgrade to Laravel 10 & JWT Init

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
 
 Route::prefix('employee')->group(function () {
     Route::get('/', [EmployeeController::class, 'index']);
@@ -16,4 +14,13 @@ Route::prefix('employee')->group(function () {
     Route::get('/get/{id}', [EmployeeController::class, 'get']);
     Route::post('/update/{id}', [EmployeeController::class, 'update']);
     Route::delete('/delete/{id}', [EmployeeController::class, 'delete']);
+});
+
+Route::get('user', [AuthController::class, 'user']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [AuthController::class, 'user']);
+    Route::post('logout', [AuthController::class, 'logout']);
 });
